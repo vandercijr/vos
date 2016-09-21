f46cd37639b0d1ff64f51eab89ad89b4acc26388
vanderci c junior <vandercijr@gmail.com>
2016-09-21 11:06:15 -0300

    clear some code on main function

--- source/kernel/kernel.c
+++ source/kernel/kernel.c
@@ -2,25 +2,9 @@
 
 kernel_main_func()
 {
-/*  char* video = (char *)VGA_VIDEO_MEM_ADDR;
-
-  video[0] = 'W';
-  video[2] = 'E';
-  video[4] = 'L';
-  video[6] = 'C';
-  video[8] = 'O';
-  video[10] = 'M';
-  video[12] = 'E';
-  video[14] = 0x00;
-  video[16] = 'T';
-  video[18] = 'O';
-  video[20] = 'V';
-  video[22] = 'O';
-  video[24] = 'S';
-  video[26] = 0x07;*/
   setIoAddr(_DefaultIoAddr);
 
   clear();
-  print("WELCOME TO VOS MY FIRST SO WRITTEN FOR HOBBY. I AM NOT INTENDING TO BE A NEW LINUS BUT LEARN HOW MACHINE WORKS");
+  print("WELCOME TO VOS");
 
 }
